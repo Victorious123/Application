a3b1b515f64faed3e76b65e8e3b4c276a992e15e
Victorious123 <viktori1@srv604.webhostingforstudents.com>
2023-06-09 17:51:44 -0400

    added image placeholder to summary page and styled it with css, added @SESSION array to paragraph for all the entered info from previous pages, added session variables in experience route to connect to @SESSION in summary, added imploding to openings route to seperate the info with quotes and commas, convert from array to string, and finally post it on summary page.

--- index.php
+++ index.php
@@ -26,6 +26,12 @@ $f3->route('GET /', function() {
 //Define a personal information route
 $f3->route('GET|POST /personalinformation', function($f3) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION["fname"] = $_POST["fname"];
+        $_SESSION["lname"] = $_POST["lname"];
+        $_SESSION["email"] = $_POST["email"];
+        $_SESSION["state"] = $_POST["state"];
+        $_SESSION["phone"] = $_POST["phone"]; //saves these in session array & session array keeps going (except the data)
+
         $f3->reroute('experience');
     }
     $view = new Template();
@@ -34,38 +40,48 @@ $f3->route('GET|POST /personalinformation', function($f3) {
 );
 
 
-
 //Define an experience route
-$f3->route('GET|POST /personexperience', function($f3) {
+$f3->route('GET|POST /experience', function($f3) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION["text"] = $_POST["text"];
+        $_SESSION["link"] = $_POST["link"];
         $f3->reroute('openings');
     }
     $view = new Template();
     echo $view->render('views/experience.html'); //routing from index to html on any page
+//    var_dump($_SESSION);
 }
 );
 
 
 //Define an openings route (mailing list)
-$f3->route('GET|POST /newopenings', function($f3) {
+$f3->route('GET|POST /openings', function($f3) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $f3->reroute('submission'); //change
+        $jobSelected = $_POST['jobs'];
+        if(isset($jobSelected)) {
+            $_SESSION['jobs'] = implode(", ", $jobSelected);  //imploding
+        }
+//        $_SESSION['jobs'] = implode(", ", $_POST['jobs']);
+        $f3->reroute('submission');
     }
+
     $view = new Template();
     echo $view->render('views/openings.html'); //routing from index to html on any page
+    //var_dump($_SESSION);
 }
 );
 
 
 //Define a summary/submission page
-//$f3->route('GET|POST /newappsub', function($f3) {
+$f3->route('GET|POST /submission', function($f3) {
 //    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-//        $f3->reroute(''); //change
+//        $f3->reroute();
 //    }
-//    $view = new Template();
-//    echo $view->render('views/newapplicant.html'); //routing from index to html on any page
-//}
-//);
+    $view = new Template();
+    echo $view->render('views/summary.html'); //routing from index to html on any page
+}
+);
+
 
 
 //Run fat free
